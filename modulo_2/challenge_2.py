7cfeedef91ed67718c7d5d747e622d1bc733f0d5
Camila <camila.bodack@assetify.com.br>
2020-05-22 13:20:24 -0300

    update code structure according to sugestions

--- modulo_2/challenge_2.py
+++ modulo_2/challenge_2.py
@@ -2,55 +2,59 @@ from abc import ABC, abstractmethod
 
 
 class Department:
+    """
+    Class that creates department structure
+    """
+
     def __init__(self, name, code):
         self.name = name
         self.code = code
 
 
 class Employee(ABC):
-    def __init__(self, code, name, salary):
+    """
+    Abstract class that creates Employee structure
+    """
+
+    def __init__(self, code, name, salary, department):
         self.code = code
         self.name = name
         self.salary = salary
+        self.__department = department
 
     @abstractmethod
     def calc_bonus(self):
         pass
-
-    @abstractmethod
-    def get_hours(self):
-        pass
-
-
-class Manager(Employee):
-    def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.__departament = Department("managers", 1)
-
-    def get_departament(self):
-        return self.__departament.name
-
-    def set_departament(self, new_department):
-        self.__departament.name = new_department
-
-    def calc_bonus(self):
-        return self.salary * 0.15
 
     def get_hours(self):
         return 8
 
+    def get_department(self):
+        return self.__department.name
 
-class Seller(Manager):
+    def set_department(self, new_department):
+        self.__department.name = new_department
+
+
+class Manager(Employee):
+    """
+    Manager class which return calc_bonus used for managers only
+    """
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.__departament = Department("sellers", 2)
+        super().__init__(code, name, salary, Department("managers", 1))
+
+    def calc_bonus(self):
+        return self.salary * 0.15
+
+
+class Seller(Employee):
+    """
+    Seller class allows to get and put sellers
+    quantities and calcuted its specific bonus
+    """
+    def __init__(self, code, name, salary):
+        super().__init__(code, name, salary, Department("sellers", 2))
         self.__sales = 0
-
-    def get_departament(self):
-        return self.__departament.name
-
-    def set_departament(self, new_department):
-        self.__departament.name = new_department
 
     def get_sales(self):
         return self.__sales
