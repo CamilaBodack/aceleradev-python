fd05622af9e0bfc6cdf8f6cb5799e1c8277dcce4
Camila <camila.bodack@gmail.com>
2020-06-30 15:30:08 -0300

    add recourses and methods

--- modulo_8/main.py
+++ modulo_8/main.py
@@ -1,4 +1,52 @@
 
-doc = '''
+doc = """
+title: API Modulo 8
+baseUri: http://modulo8.com/{version}
+version: v1
+mediaType: aplication/json
 
-'''
+types:
+  Auth:
+    type: object
+    discriminator: token
+    properties:
+      token : string
+  Agent:
+    type: object
+
+/auth/token:
+  post: #Solicita um token para API
+
+/agents: #Recursos de agentes
+  post: #Cria um agente
+  get: #Lista todos agentes
+  /{id}:
+    delete: #Remove o agente pelo id
+    put: #Atualiza os dados do usuario pelo id
+  /{id}/events:
+    post: #Cria um evento
+    get: #Lista o evento
+    delete: #Remove o evento pelo id
+    put: #Atualiza o evento pelo id
+  /{id}/events/{id}:
+     post: #Cria um evento
+     get: #Lista o evento
+     delete: #Remove o evento pelo id
+     put: #Atualiza o evento pelo id
+
+/groups: #Recursos de grupos
+  post: #Cria um grupo
+  get: #Lista todos grupos
+  /{id}:
+    delete: #Remove um grupo pelo id
+    put: #Atualiza o grupo pelo id
+
+/users: #Recursos de usuarios
+  post: #Cria um usuario
+  get: #Lista todos usuarios
+  /{id}:
+    delete: #Remove o usuario pelo id
+    put: #Atualiza os dados do usuario pelo id
+
+
+"""
