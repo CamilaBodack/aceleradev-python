da903279cbfa676a0f15831c621ce22b90fd6a94
Camila <camila.bodack@gmail.com>
2020-07-06 19:34:58 -0300

    add .travis config

--- modulo_9/main.py
+++ modulo_9/main.py
@@ -1,4 +1,18 @@
 # .travis.yml
 config = """
+language: python
+python:
+    - "2.7"
+    - "3.7"
+    - "7.1.1"
+    - "7.1.1-beta0"
+    - "pypy"
+    - "pypy3"
+
+
+install:
+    - pip install -r requirements.txt
+
+script: pytest
 
 """
