3bfb460d25572f81e460b30c41ea940170c2e66d
Camila <camila.bodack@gmail.com>
2020-07-06 21:49:48 -0300

    update travis config to generic deploy

--- modulo_9/main.py
+++ modulo_9/main.py
@@ -4,15 +4,20 @@ language: python
 python:
     - "2.7"
     - "3.7"
-    - "7.1.1"
-    - "7.1.1-beta0"
     - "pypy"
     - "pypy3"
 
+stages:
+    - name: test
+    - name: deploy
 
 install:
     - pip install -r requirements.txt
 
 script: pytest
+
+jobs:
+    - stage: test
+    - stage: deploy
 
 """
